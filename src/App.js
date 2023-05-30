39cbed7d22b43a33423c497c3c103102364385fc
dmoreir1 <dmoreir1@binghamton.edu>
2023-05-29 22:16:19 -0400

    calculator functioning

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react'
 import './styles.css'
 
+const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
+
+
+function reducer (state, {action}) {
+  switch(type) {
+    case ACTIONS.ADD_DIGIT:
+    return {
+      ...state,
+      currentOperand: `${currentOperand || "" }${payload.digit}`
+    }
+  }
+}
+
+
 function App() {
+  const [{currentOperand, previousOperand, operation}, dispacth] = useReducer(reducer, {})
+
+  dispacth({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1}})
+
   return( 
      <div className = 'calculator-grid'>
       <div className="output">
-        <div className="previous-operand">6969</div>
-        <div className="current-operand">1337</div>
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
 
       <button className="span-two">AC</button>
